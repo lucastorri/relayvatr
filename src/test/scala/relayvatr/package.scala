cd0d9f0eb06e0e17c927728a1e7ea21f6a2b6b17
Lucas Torri <lucastorri@gmail.com>
2016-02-06 20:22:01 +0100

    increase test timeout

--- src/test/scala/relayvatr/package.scala
+++ src/test/scala/relayvatr/package.scala
@@ -18,7 +18,7 @@ package object relayvatr {
       executeWithTimestamps(trip).map(_.event)
 
     def executeWithTimestamps(trip: UserTrip): Seq[TimestampedEvent] = {
-      Await.ready(trip.on(system), 5.seconds)
+      Await.ready(trip.on(system), 10.seconds)
       collected.toSeq
     }
 
