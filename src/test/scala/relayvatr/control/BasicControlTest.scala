2b26e53b240ea37945ddbeb0e6f10719e85eeb0b
Lucas Torri <lucastorri@gmail.com>
2016-02-07 01:56:40 +0100

    reenable control test

--- src/test/scala/relayvatr/control/BasicControlTest.scala
+++ src/test/scala/relayvatr/control/BasicControlTest.scala
@@ -1,29 +1,29 @@
 package relayvatr.control
 
 import relayvatr._
-import relayvatr.event.{Action, ActionEvent, SystemEvent, SystemShutdown}
+import relayvatr.event._
 import relayvatr.scheduler.Scheduler
 import rx.lang.scala.Observable
 import rx.lang.scala.subjects.ReplaySubject
 
 class BasicControlTest extends Test {
 
-//  it must "generate a shutdown event" in {
-//    val events = ReplaySubject[SystemEvent]()
-//    val system = new BasicControl(NoOpScheduler)
-//
-//    system.events.subscribe(events)
-//    system.shutdown()
-//
-//    events.toBlocking.toIterable.toSeq must equal (Seq(
-//      SystemShutdown
-//    ))
-//  }
-//
-//  object NoOpScheduler extends Scheduler {
-//    override def events: Observable[ActionEvent] = Observable.just()
-//    override def shutdown(): Unit = {}
-//    override def handle(action: Action): Unit = {}
-//  }
+  it must "generate a shutdown event" in {
+    val events = ReplaySubject[SystemEvent]()
+    val system = new BasicControl(NoOpScheduler)
+
+    system.events.subscribe(events)
+    system.shutdown()
+
+    events.toBlocking.toIterable.toSeq must equal (Seq(
+      SystemShutdown
+    ))
+  }
+
+  object NoOpScheduler extends Scheduler {
+    override def events: Observable[ElevatorEvent] = Observable.just()
+    override def shutdown(): Unit = {}
+    override def handle(action: Action): Unit = {}
+  }
 
 }
